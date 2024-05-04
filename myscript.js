327e193dd9e6d6be894d3ed1191088ba41fe93ee
MykolaKytsyshyn <kytsyshyn.m@gmail.com>
2024-05-04 13:38:55 +0300

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,4 @@
+document.getElementById('1').src = 'https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-980x653.jpg';
+document.getElementById('2').src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2mpnEBam3dC0D8wNKLCjE8jJJkj5zEF57xHKp4A57yQ&s';
+document.getElementById('3').src = 'https://kolo.news/media/news_watermark/34547.jpg?v=144564';
+document.getElementById('4').src = 'https://i.lb.ua/098/25/5d7d26300131f.jpeg';
